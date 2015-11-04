2391407361ec4a5a606a3ce912f8c63f801a3860
Mermouy <mermouy+kaos@gmail.com>
2015-11-04 06:09:25 +0100

    Déplacement de locale.conf

--- install.sh
+++ install.sh
@@ -8,12 +8,13 @@ TargetDir="${HOME}"
 
 ## Bash files
 _bashinstall() {
-    cat bashrc ${1}/${1}.bashrc > ${TargetDir}/.bashrc || return 1# Copy bashrc
+    cat bashrc ${1}/${1}.bashrc > ${TargetDir}/.bashrc || return 1	# Copy bashrc
     cat bash_aliases ${1}/${1}_aliases perso/perso_aliases > ${TargetDir}/.bash_aliases || return 1    # Create .bash_aliases
     cat bash_functions ${1}/${1}_functions perso/perso_functions > ${TargetDir}/.bash_functions || return 1    # Create .bash_functions
     if [ -f "${1}/post-${1}-install.sh" ]   # If exists, run post-install script
-        then bash -c "${1}/post-${1}-install.sh" || return 1
+        then ./${1}/post-${1}-install.sh || return 1
     fi
+    return 0
 }
 
 
@@ -23,7 +24,8 @@ if [ -z "${1}" ];then   # If there's argument
 elif [ ! -d "${1}" ];then   # If argument exist as a folder
     echo -e "ERROR:\nArgument is not an existing folder... (There's no "${1}" in $(basedir ${0}))...\nExiting..."
 else
-    [ $(_bashinstall "${1}") ] || echo -e 'Shit happened!!! Sorry...'
+    _bashinstall "${1}"
+    if [ "${?}" = "1" ];then echo -e 'Shit happened!!! Sorry...';fi
 fi
 
 echo -e 'Dot files installed!\nBye!'
