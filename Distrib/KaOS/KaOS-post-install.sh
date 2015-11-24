fd2733c4ecfb4394b92552fc463e5d6a7fce26d9
Mermouy <mermouy+kaos@gmail.com>
2015-11-24 10:24:51 +0100

    Post install soon running

--- Distrib/KaOS/KaOS-post-install.sh
+++ Distrib/KaOS/KaOS-post-install.sh
@@ -4,22 +4,137 @@ TargetDir="${HOME}"
 K_Dir="$(dirname $0)"
 RunDir="$(dirname ${K_Dir})"
 Distrib=KaOS
+IFile=${TargetDir}/.config/KaOS-restore.log
+_pac="pacman"
+Dial="kdialog --title=$(basename $0 .sh)"
+yad="yad --icon-theme=midna --window-icon=kaos.svg --title=$(basename $0 .sh) --center --text-align=fill --selectable-labels"
 
-if [ ! -f ${TargetDir}/.config/${Distrib}-post-install ]
-then
-Dial='kdialog --title "KaOS-post-installation script" --icon=/usr/share/icons/KaOS.svg'
-${Dial} --yesno "Edit your pacman and mirrorlist files (you can safely skip this phase if you are in USA)"
+# Make copy with .ori backup
+cpb() { cp -v $@{,.ori} ;}
+
+### Functions
+_error() {
+        kdialog --error "$1"
+}
+
+_errorexit() {
+        kdialog --error "$1" && exit 0;
+}
+
+# Verify actual state of pkgs
+appstatus() {
+        [ -n "$(${_pac} -Qq $1 2>/dev/null)" ] && echo $1 # If present return pkg name
+}
+
+pstatus() {
+        [ -n "$(${_pac} -Qq $1 2>/dev/null)" ] && echo FALSE || echo TRUE
+}
+
+ostatus() {
+         [ -z "$(kcp -Nl | grep -e "^$1$" 2>/dev/null)" ] && echo 'PACMAN octopi-panel' || echo 'KCP /usr/share/icons/octopi.png'
+}
+
+FirstUpdate() {
+# Ask for /etc/pacman.conf & mirrorlist editing
+        ${Dial} --yesno "Do you want to edit your pacman and mirrorlist files to make first update faster?\n(you can safely skip this phase if you are in USA)\nThis script will continue when you close the editor."
         if [ "$?" = "0" ]
         then
+                cpb /etc/mirrorlist /etc/pacman.conf
                 kdesu kate /etc/mirrorlist /etc/pacman.conf
         fi
 
-### Updates 
-        _pac="pacman"
-        ${_pac} -Suy && touch ${TargetDir}/.config/${Distrib}-post-install
-        ${Dial} "msgbox 'You should reboot now to be sure to run an updated system...' --oklabel 'Reboot'" && reboot
+### Update
+# Should work with fifo       Fifo=$(mktemp /tmp/KaOS-restore.XXX)
+# TODO: Better control of the update/install process
+        kdesu -c "konsole -e mirror-check" -c "konsole -e ${_pac} -Suyy" 
+        read -p 'Wait for update to finnish then hit enter:' && echo -e "$(date -I)\nupdate" >> $IFile || _error "Problem encountered while updating system"
+
+# Needed lib for these scripts to run
+        Basepkglist="base-devel kcp gtk3 hicolor-icon-theme intltool"
+# Ask for installation
+        ${Dial} --yesno "This script need these few librairies to run at is best: <p><b>${Basepkglist}</b><p>Should we install them now? (Script will end if not...)" --yes-label "Install" --no-label "Cancel & exit"
+        [ $? != 0 ] && exit 0 # Quit if not cause we need these libs!
+
+        # Force Update before install
+        kdesu -c "konsole -e  ${_pac} -Syy --noconfirm && ${_pac} -S ${Basepkglist} --needed --noconfirm"
+        read -p "Wait for install to finnish then hit enter: " 
+         
+        # Kcp app list (only what we really need for these scripts)
+        KcPkgList="yad"
+        
+        # Kcp update & apps installation
+        kcp -u >/dev/null  && ${Dial} --msgbox="<b>Kcp database has been updated.</b><p>Here are some packages we need to install from kcp: <p><code>${KcPkgList}</code>" --yes-label="Install"
+        [ $? != 0 ] && exit 0 # Quit if not cause we need these libs!
+        konsole -e "kcp -i ${KcPkgList}" 2>/dev/null && read -p "Wait for install to finnish then hit enter: " 
+                
+        # Ask for reboot
+        ${Dial} --yesno "System has been updated, should we\nREBOOT ?\n(Not needed if you updated just a few packages...)" --yes-label "Reboot" --no-label "Not now"
+# would permit:       ${Dial} --textbox ${Fifo} "Once finished you should reboot in order to run an updated system...\nYou will need to run this script again without that first step.' --oklabel 'Reboot'" && rm -vf /tmp/KaOS-restore.* 
+        [ $? = 0 ] && reboot
+        return 0
+}
+
+# Verify apps status, do not propose them for install again
+ToInstall() {
+        # File containing app list
+        t_file="${K_Dir}/${Distrib}-pacman-list.txt"
+        # All needed for progress bar status
+        nbpkg=$(wc -l< ${t_file}) # Number of applications found
+        ppb=$(echo "scale=9; 100.0/${nbpkg}" | bc) # Divider
+        # Keep one non-variable
+        pp="${ppb}"
+        rm -f /tmp/KaOS-restore.tmp
+        for app in $(tr '\n' ' ' < ${t_file})
+        do
+                # First echo to file
+                echo "$(pstatus ${app}) ${app} ${app} $(ostatus ${app}) " >> /tmp/KaOS-restore.tmp
+                # Second to progress bar
+                echo "$(pstatus ${app}) ${app} $(ostatus ${app})"
+                echo "${pp} ${pp}%: Verify ${app}"
+                pp=$(echo "scale=9; ${pp} + ${ppb}" | bc)
+        done< ${t_file}
+}
+        
+PkgPostInstall() {
+        # Verify yad is installed
+        $(pacman -Qq yad >/dev/null 2>&1) || _errorexit "This script need <b>yad</b> to be installed.<p>Maybe some problem occured while installing it, please install it manually before running this script again.<p>For this, simply run in a terminal: <p><code>kcp -u && kcp -i yad</code>"
+        
+        ToInstall | ${yad} --progress --percentage=1 --auto-close 
+
+        # Permit user select and deselect apps
+        Pacpkg=$(${yad} --list --checklist --text="Deselect now applications you don't want to be added to your system: \nAlready installed packages are unchecked, and won't be removed, to remove them do it manually with root privileges like: \npacman -R 'Your packages'" --column="Select" --column="Application" --column=Icon:IMG --column="Origin" --column="Icon Origin":IMG --image=octopi-panel --image-on-top --height 700 --width 500 $(tr '\n' ' ' < /tmp/KaOS-restore.tmp) )
+        
+        paclist=$(echo "${Pacpkg}" | grep PACMAN | sed -e 's/|PACMAN||//g;s/TRUE|//g' | sed 's/|//' | tr '\n' ' ')
+        
+        kcplist=$(echo "${Pacpkg}" | grep KCP | sed -e 's/|KCP||//g;s/$|//g')
+        
+        # Install pacman packages
+        [ -n "${paclist}" ] && kdesu -c "konsole -e ${_pac} --noconfirm -S $(echo ${paclist})" && read -p "Wait for package install to finish then hit enter: "
+        
+        # Install kcp  packages
+        [ -n "${kcplist}" ] && konsole -e "kcp -s $(echo ${kcplist}) && read -p 'Wait for kcp install to finish then hit enter: '" 
+        
+        # Keep trace this step has been run
+        echo "post" >> ${IFile}
+}
+
+### Main script launching Functions
+# Verify if first update has been run 
+if [ ! -f ${IFile} ] || [ -z "$(grep "update" ${IFile})" ];then
+        FirstUpdate
+else
+        # Else ask for rerun?
+        ${Dial} --yesno "First update has been run do you want to run it again?"
+        [ $? = 0 ] && FirstUpdate
 fi
 
-ToInstall="$(cat ${K_Dir}/${Distrib}-pacman-list.txt)"
-kdialog --checklist "Select or deselect now applications you want to be added to your system: " ${ToInstall}
-        
+# Verify if post installation pkg have been installed
+if [ -z "$(grep "post" ${IFile})" ] || [ "$1" = "--postinstall" ];then
+        PkgPostInstall || _errorexit "Error occured while post installing packages."
+else
+        # Else ask for rerun?
+        ${Dial} --yesno "This post installation has already been run should we run it again?"
+        if [ $? = 0 ];then
+                PkgPostInstall || _errorexit "Problem occured while installing packages."
+        fi
+fi
--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@
 
 TargetDir="${HOME}"
 RunDir="$(dirname $0)"
+Confile="$HOME/.config/KaOS-restore.log"
+
+echo "">>${Confile} || exit 1
 
 # For now this just verify we are on a KaOS system but it shoud next identify which distrib it is
 if [ -f /etc/lsb-release ]
@@ -25,29 +28,29 @@ _error() {
 }
 
 _errorexit() {
-        kdialog --error "$1" && exit 0
+        kdialog --error "$1" && exit 0;
 }
 
 _postinstall() {
 # Once identified it should run the appropriate distrib specific install-script
 case $1 in 
-        KaOS)DistribDir="${RunDir}/Distrib/KaOS"; [ -f ${DistribDir}/KaOS-post-install.sh ] && chmod +x ${DistribDir}/KaOS-post-install.sh && K_postinstall || exit 1;;
+        KaOS)DistribDir="${RunDir}/Distrib/$1"
+                [ -f ${DistribDir}/$1-post-install.sh ] && chmod +x ${DistribDir}/$1-post-install.sh && $1_postinstall || exit 1;;
         Manjaro) echo "Doing Manja stuff";;
         *) echo "Distribution not (yet?) supported" && return 1;;
 esac
 }
-export -f _postinstall
+export _postinstall
 
-K_postinstall() {
+KaOS_postinstall() {
 # Not sure how it should be launch...
-kdialog --title "Run this KaOS Dotfiles Restore script?" --textbox "${DistribDir}/KaOS-post-install.sh" 800 600
-# Run install script?
-[ "$?" = "0" ] && bash -c ${DistribDir}/KaOS-post-install.sh || return 1
+[ "$?" = "0" ] && bash -x ${DistribDir}/KaOS-post-install.sh || return 1
 }
 
 ## Bash files
 _bashinstall() {
 # For now only restore few dotfiles but then should ask for lib to use etc...
+        read
     cat bashrc ${1}/${1}.bashrc > ${TargetDir}/.bashrc || return 1	# Copy bashrc
     cat bash_aliases ${1}/${1}_aliases perso/perso_aliases > ${TargetDir}/.bash_aliases || return 1    # Create .bash_aliases
     cat bash_functions ${1}/${1}_functions perso/perso_functions > ${TargetDir}/.bash_functions || return 1    # Create .bash_functions
@@ -62,7 +65,7 @@ _bashinstall() {
 _etcinstall() {
     # Need root privileges
     if [[ ${EUID} -ne 0 ]]; then
-	echo -e "\e[31mThis script must be run by root!\nExiting now...\e[0m"
+	echo -e "\e[31mThis script must be run by root\!\nExiting now...\e[0m"
 ﻿	exit 1
     fi
     rsync -r -x -c -b -i -s etc/ /etc/ || return 1
@@ -79,7 +82,6 @@ _gitconf() {
 }
 
 _configinstall() {
-
 # VARIABLES
 Savef=mermouy.dot
 Sext=tar.bz2
@@ -96,24 +98,37 @@ esac
 
 ### TODO: Other dot files
 
-### TODO: confirmation dialog (qarma?)
-
 # .config dir TODO:and personnal stuff
 rsync --remove-source-files .config/ ${HOME}/.config/ && find -type d -empty -delete
 return 0
 }
 
+# Check if step has been done
+_istatus() {
+        [ -n "$(grep $1 ${Confile})" ] && echo off || echo on
+}
+
 # Control what step to do
 Steps=( '_postinstall' '_bashinstall' '_configinstall' '_etcinstall' )
-# Just a translation for humans
-Act=( 'Post Installation and update.' 'Bash files installation' 'Config files and personnal files restoration.' 'System files restoration' )
-Steps2do=$(kdialog --title "What should I do?" --checklist "Choose the step(s) which should be run: " _postinstall 'Post Installation and update.' on _bashinstall 'Bash files installation' on )
+Steps2do=( $(kdialog --title "What should we do?" --checklist "Choose the step(s) which should be run: " _postinstall 'Post Installation and update.' $(echo $(_istatus post)) _bashinstall 'Bash files installation' $(echo $(_istatus bash)) _configinstall 'Config files and personnal files restoration.' $(echo $(_istatus perso)) _etcinstall 'System files restoration' $(echo $(_istatus sys)) 2>/dev/null) )
 
-case "$1" in
-        "") for a in ${Steps2do[@]}; do ${a} ${Distrib}) || _errorexit "Error while ${a}"; done
-        read;;
-        "--all|-a") for a in ${Steps2do[@]}${Act[@]}; do ${a} ${Distrib} || _error "Errors happened while ${Act}";done ;;
-        "--postinstall|-p") for Act in "_bashinstall  _configinstall _etcinstall|-e" ; do $(${Act}) ${Distrib} || _error "Errors happened while ${Act}";done ;;
-        "system|-s") _etcinstall ${Distrib} || _error "Errors happened";;
-        *) echo "...Ok...";;
+case $1 in
+        --postinstall|-p) 
+        for Act in "_bashinstall  _configinstall _etcinstall" 
+                do ${Act} ${Distrib} || _error "Errors happened while ${Act}"
+        done 
+        ;;
+        --config|-c)  
+        for Act in " _configinstall _etcinstall" 
+                do $(${Act}) ${Distrib} || _error "Errors happened while ${Act}"
+        done 
+        ;;
+        system|-s)
+        _etcinstall ${Distrib} || _error "Errors happened"
+        ;;
+        *) 
+        for a in ${Steps2do[@]}
+                do $(eval ${a} ${Distrib}) || _error "Error while ${a}"
+        done
+        ;;
 esac
