7af3be1977654b85d8d74a78411c1bdb883015fb
Mermouy <mermouy+kaos@gmail.com>
2015-11-29 18:35:39 +0100

    Add pkg post-install script

---
+++ Distrib/KaOS/KaOS-post-pkg-install.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Mini script to install some some extra packages after KaOS install
+_pac="pacman"
+Dial='kdialog --title "Post Install Extra Packages" --icon "kaos.svg"'
+### Util-Functions
+# verify package status
